9672ae626ca03524ca28c1b9b0bb1d1dbac93354
Thomas <thomas.drouvin@supertripper.com>
2021-07-31 12:07:53 +0200

    Added 5  objects to  var pokemonList with parameters

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Displays list of Pokemon with the following:
 1. Name
 2. Description
 3. Type
-4. Special Power
+4. Height
 
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-let pokemonList = [{}, {}]
+let pokemonList = [
+  { pokename: 'Bulbasaur', height: 7, types: ['Grass', 'Poison'] },
+  { pokemon: 'Squirtle', height: 5, types: 'Water' },
+  { pokemon: 'Wigglytuff', height: 1, types: ['Fairy', 'Normal'] },
+  { pokemon: 'Slowpoke', height: 1.2, types: ['Psychic', 'Water '] },
+  { pokemon: 'Dewgong', height: 1.7, types: ['Ice', 'Water'] },
+
+];,
