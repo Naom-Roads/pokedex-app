678130579225c7f054d8ea4e6581c3a45ef9e541
root <root@DESKTOP-64T0Q7O.localdomain>
2021-08-08 14:24:25 +0200

    Added for loop to iterate over pokemon list and display name and height

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
-let pokemonList = [
-  { pokename: 'Bulbasaur', height: 7, types: ['Grass', 'Poison'] },
-  { pokemon: 'Squirtle', height: 5, types: 'Water' },
-  { pokemon: 'Wigglytuff', height: 1, types: ['Fairy', 'Normal'] },
-  { pokemon: 'Slowpoke', height: 1.2, types: ['Psychic', 'Water '] },
-  { pokemon: 'Dewgong', height: 1.7, types: ['Ice', 'Water'] },
+let pokemon = [
+  { name: 'Bulbasaur', height: 7, types: ['Grass', 'Poison'] },
+  { name: 'Squirtle', height: 5, types: 'Water' },
+  { name: 'Wigglytuff', height: 1, types: ['Fairy', 'Normal'] },
+  { name: 'Slowpoke', height: 1.2, types: ['Psychic', 'Water '] },
+  { name: 'Dewgong', height: 1.7, types: ['Ice', 'Water'] },
 
 ];
+
+for ( let i=0; i < pokemon.length ; i++ ) {
+  document.write(" " + pokemon[i].name + " (height: " + pokemon[i].height + ")") ;
+}
