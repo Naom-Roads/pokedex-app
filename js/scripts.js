91b11d746e6fa653720bb037dea22b1e9a2771e6
Naom-Roads <nrrodrig@gmail.com>
2021-08-10 10:03:29 +0200

    Added a few more pokemon and created arrays based on type and modified function with placeholder parameter

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,38 @@
-let pokemonList = [
-  { pokename: 'Bulbasaur', height: 7, types: ['Grass', 'Poison'] },
-  { pokemon: 'Squirtle', height: 5, types: 'Water' },
-  { pokemon: 'Wigglytuff', height: 1, types: ['Fairy', 'Normal'] },
-  { pokemon: 'Slowpoke', height: 1.2, types: ['Psychic', 'Water '] },
-  { pokemon: 'Dewgong', height: 1.7, types: ['Ice', 'Water'] },
 
+
+const waterPokemon = [
+  { name: 'Squirtle', height: 5, types: 'Water' },
+  { name: 'Dewgong', height: 1.7, types: ['Ice', 'Water'] },
+  { name: 'Slowpoke', height: 1.2, types: ['Psychic', 'Water '] },
 ];
+
+const fairyPokemon = [
+{ name: 'Alcremie', height: 1, types: 'Fairy' },
+{ name: 'Igglybuff', height: 1, types: 'Fairy' },
+];
+
+const grassPokemon = [
+  { name: 'Bulbasaur', height: 7, types:['Grass', 'Poison'] },
+  { name: 'Chikorita', height: 2.11, types: 'Grass' },
+];
+
+
+
+// Iterates through list and checks to see if height is greater than 5, then it displays the name and height
+
+
+function printArray(list) {
+  for (let i=0; i < list.length; i++) {
+    if (list[i].height > 5) {
+      document.write("<div class='grid-item'> " + list[i].name + " (Height: " + list[i].height + ") " + "<span> Wow that is a big pokemon! </span></div> <br>");
+    } else {
+      document.write("<div class='grid-item'> " + list[i].name + " (Height: " + list[i].height + ")</div> <br>");
+    }
+  }
+}
+
+document.write("<div class='grid'>");
+printArray(waterPokemon);
+printArray(fairyPokemon);
+printArray(grassPokemon);
+document.write("</div>");
