067a66d48df6edaedeff85a26cab839efa0e08ee
Naom-Roads <nrrodrig@gmail.com>
2021-08-11 13:03:00 +0200

    Wrapped IIFE in variable pokemonRepository and called it in forEach function to display Pokemon and show the one with a height greater than 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,46 @@
 
+// Iterates through list and checks to see if height is greater than 5
+// Displays the name and height 
 
-const pokemonList = [
+const pokemonRepository = (function() {
+let pokemonList = [
   {name: 'Squirtle', height: 5, types: 'Water' },
   {name: 'Dewgong', height: 1.7, types: ['Ice', 'Water'] },
   {name: 'Slowpoke', height: 1.2, types: ['Psychic', 'Water '] },
-
   {name: 'Alcremie', height: 1, types: 'Fairy' },
   {name: 'Igglybuff', height: 1, types: 'Fairy' },
-
   {name: 'Bulbasaur', height: 7, types:['Grass', 'Poison'] },
   {name: 'Chikorita', height: 2.11, types: 'Grass' },
-
 ];
 
-// Iterates through list and checks to see if height is greater than 5, then it displays the name and height
+function add(pokemon) {
+pokemonList.push(pokemon);
+}
+
+function getAll() {
+  return pokemonList; 
+  }
+
+  return {
+    add: add, 
+    getAll: getAll
+
+};
+
+})();  
+
 document.write("<div class='grid'>");
 
-  pokemonList.forEach(function(pokemon) {
-    const pokemonIsTall = pokemon.height > 5; 
-      document.write(`<div class='grid-item'> ${pokemon.name} (Height: ${pokemon.height})
-      ${pokemonIsTall ? "<span> Wow that is a big pokemon! </span>" : ""} </div> <br>`);
- });
+pokemonRepository.getAll().forEach(function(pokemon) {
+  const pokemonIsTall = pokemon.height > 5; 
+    document.write(`<div class='grid-item'> ${pokemon.name} (Height: ${pokemon.height})
+    ${pokemonIsTall ? "<span> Wow that is a big pokemon! </span>" : ""} </div> <br>`);
+});
+
+document.write();
+
 document.write("</div>");
+
+
+
+
