acdf10aaf0bb7d840378c24e6c85900f3821c8f4
Naom-Roads <nrrodrig@gmail.com>
2021-08-12 13:23:02 +0200

    Added function addListItem that adds list items with the class of button, also added a few styling changes in CSS file, as well as a normalize css file

--- js/scripts.js
+++ js/scripts.js
@@ -2,49 +2,68 @@
 // Iterates through list and checks to see if height is greater than 5
 // Displays the name and height 
 
-const pokemonRepository = (function() {
-let pokemonList = [
-  {name: 'Squirtle', height: 5, types: 'Water' },
-  {name: 'Dewgong', height: 1.7, types: ['Ice', 'Water'] },
-  {name: 'Slowpoke', height: 1.2, types: ['Psychic', 'Water '] },
-  {name: 'Alcremie', height: 1, types: 'Fairy' },
-  {name: 'Igglybuff', height: 1, types: 'Fairy' },
-  {name: 'Bulbasaur', height: 7, types:['Grass', 'Poison'] },
-  {name: 'Chikorita', height: 2.11, types: 'Grass' },
-];
+const pokemonRepository = (function () {
+  let pokemonList = [
+    { name: 'Squirtle', height: 5, types: 'Water' },
+    { name: 'Dewgong', height: 1.7, types: ['Ice', 'Water'] },
+    { name: 'Slowpoke', height: 1.2, types: ['Psychic', 'Water '] },
+    { name: 'Alcremie', height: 1, types: 'Fairy' },
+    { name: 'Igglybuff', height: 1, types: 'Fairy' },
+    { name: 'Bulbasaur', height: 7, types: ['Grass', 'Poison'] },
+    { name: 'Chikorita', height: 2.11, types: 'Grass' },
+  ];
 
-function add(pokemon) {
-pokemonList.push(pokemon);
-}
 
-function getAll() {
-  return pokemonList; 
+
+  function add(pokemon) {
+    if (typeof pokemon === "object") {
+      pokemonList.push(pokemon);
+    }
   }
 
+  function getAll() {
+    return pokemonList;
+  }
+
+
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector('.pokemon-list');
+    let pokemonItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('button-class');
+    pokemonItem.appendChild(button);
+    pokemonList.appendChild(pokemonItem);
+  }
+
+
   return {
-    add: add, 
-    getAll: getAll
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem
 
-};
 
-})();  
+  };
 
-console.log(pokemonRepository.getAll()); 
+
+})();
+
+console.log(pokemonRepository.getAll());
 pokemonRepository.add({ name: 'Butterfree', height: 3, type: 'Bug' });
+pokemonRepository.add("coucou"); // testing pokemon is not an object 
 console.log(pokemonRepository.getAll());
 
-
-document.write("<div class='grid'>");
-pokemonRepository.getAll().forEach(function(pokemon) {
-  const pokemonIsTall = pokemon.height > 5; 
-    document.write(`<div class='grid-item'> ${pokemon.name} (Height: ${pokemon.height})
-    ${pokemonIsTall ? "<span> Wow that is a big pokemon! </span>" : ""} </div> <br>`);
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
 });
 
-document.write();
 
-document.write("</div>");
 
+
+
+// const pokemonIsTall = pokemon.height > 5; 
+    // document.write(`<div class='grid-item'> ${pokemon.name} (Height: ${pokemon.height})
+    // ${pokemonIsTall ? "<span> Wow that is a big pokemon! </span>" : ""} </div> <br>`);
 
 
 
