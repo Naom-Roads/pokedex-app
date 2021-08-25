cdca76f3a025d03bd876278b26db0dcc0c7b3a21
Naom-Roads <nrrodrig@gmail.com>
2021-08-25 10:11:04 +0200

    Removed # in link to fix the limit of pokemon shown

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
 
 const pokemonRepository = (function () {
   let pokemonList = [];
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit#=150';
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   // LOADING MESSAGE //
 
@@ -115,7 +115,6 @@ const pokemonRepository = (function () {
 
       const titleElement = document.createElement('h1');
       titleElement.innerText = pokemon.name;
-      
       
       const imageElement = document.createElement('img');
       imageElement.src = pokemon.imageURL;
