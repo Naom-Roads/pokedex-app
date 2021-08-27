baa0f9098ec8245e8aa1ae03a13c40a3d1394bd8
Naom-Roads <nrrodrig@gmail.com>
2021-08-27 11:42:51 +0200

    Added a bit of styling to text for links

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
 
 const pokemonRepository = (function () {
   let pokemonList = [];
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150&offset=150';
 
   // LOADING MESSAGE //
 
